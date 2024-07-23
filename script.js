c6bfd755afe22b0dcb08270df011ca63975b5301
archivedghost001 <aryamuhammadkamil@gmail.com>
2024-07-23 20:20:29 +0700

    Final

--- script.js
+++ script.js
@@ -4,10 +4,11 @@ const currentYear = new Date().getFullYear();
 document.getElementById("currentYear").textContent = currentYear;
 
 // this part to connected to google spreadsheets
-const scriptURL = "";
+const scriptURL =
+  "https://script.google.com/macros/s/AKfycbw7If9A7zMjnDCeVW1xCR-HOQUxF49ruAc_EqdrCHOx6kCGVgzL8K2ZZJ0FbyfVH-03/exec";
 const form = document.forms["arya-contact-me"];
 const btnKirim = document.querySelector(".btn-kirim");
-const loading = document.querySelector(".btn-loading");
+const btnLoading = document.querySelector(".btn-loading");
 const myAlert = document.querySelector(".my-alert");
 
 form.addEventListener("submit", (e) => {
@@ -16,4 +17,16 @@ form.addEventListener("submit", (e) => {
   // when submit presse, showing loading btn, then remove btn Kirim
   btnLoading.classList.toggle("d-none");
   btnKirim.classList.toggle("d-none");
+  fetch(scriptURL, { method: "POST", body: new FormData(form) })
+    .then((response) => {
+      // tampilkan tombol kirim, hilangkan loading
+      btnLoading.classList.toggle("d-none");
+      btnKirim.classList.toggle("d-none");
+      // tampilkan aler/pesan success
+      myAlert.classList.toggle("d-none");
+      // reset form
+      form.reset();
+      console.log("Success!", response);
+    })
+    .catch((error) => console.error("Error!", error.message));
 });
